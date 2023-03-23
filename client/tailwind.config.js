5e908887c36d71f072420cf0dd225508c9ed96ef
Chijioke Chika <8pluscreative@gmail.com>
2023-03-23 22:31:27 +0100

    Setup

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  mode: "jit",
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     fontFamily: {
       Roboto: ["Roboto", "sans-serif"],
