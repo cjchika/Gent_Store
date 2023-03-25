f4579ae49fa6007abc76716049a808b18244253d
Chijioke Chika <8pluscreative@gmail.com>
2023-03-25 19:56:13 +0100

    User

--- server/handlers/sendMail.js
+++ server/handlers/sendMail.js
@@ -1,7 +1,29 @@
 import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const sendMail = async (options) => {
   const transporter = nodemailer.createTransport({
-    //
+    service: "gmail",
+    auth: {
+      type: "OAuth2",
+      user: process.env.MAIL_USERNAME,
+      pass: process.env.MAIL_PASSWORD,
+      clientId: process.env.OAUTH_CLIENTID,
+      clientSecret: process.env.OAUTH_CLIENT_SECRET,
+      refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+    },
   });
+
+  const mailOptions = {
+    from: process.env.MAIL_USERNAME,
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+  };
+
+  await transporter.sendMail(mailOptions);
 };
+
+export default sendMail;
