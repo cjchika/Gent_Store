f7fba3f191d66f40829687023686a3562164be01
Chijioke Chika <8pluscreative@gmail.com>
2023-03-26 19:55:18 +0100

    User

--- server/controllers/User.js
+++ server/controllers/User.js
@@ -8,6 +8,7 @@ import fs from "fs";
 import jwt from "jsonwebtoken";
 import sendMail from "../handlers/sendMail.js";
 import { userAuth } from "../middleware/Auth.js";
+import { sendToken } from "../handlers/userToken.js";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -42,10 +43,39 @@ export const createUser = async (req, res, next) => {
     // const user = new User({ name, email, password, avatar: fileUrl });
     // const saveUser = await user.save();
     // res.status(201).json(saveUser);
-
     console.log(user);
+
+    const activationToken = createActivationToken(user);
+
+    const activationUrl = `http://localhost:5173/activation/${activationToken}`;
   } catch (error) {
     console.log(error.message);
     res.status(404).json({ message: error.message });
   }
 };
+
+export const activateUser = asyncErrors(async (req, res, next) => {
+  try {
+    const { activation_token } = req.body;
+
+    const newUser = jwt.verify(activation_token, process.env.ACTIVATION_SECRET);
+
+    if (!newUser) {
+      return next(new ErrorHandler("Invalid token", 400));
+    }
+
+    const { name, email, password, avatar } = newUser;
+
+    let user = await User.findOne({ email });
+
+    if (user) {
+      return next(new ErrorHandler("User already exists", 400));
+    }
+
+    user = await User.create({ name, email, password, avatar });
+
+    sendToken(user, 201, res);
+  } catch (error) {
+    return next(new ErrorHandler(error.message, 500));
+  }
+});
--- server/routes/User.js
+++ server/routes/User.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { createUser } from "../controllers/User.js";
+import { createUser, activateUser } from "../controllers/User.js";
 // import { authToken } from "../middleware/Auth.js";
 import { upload } from "../multer.js";
 
 const router = express.Router();
 
 router.post("/create-user", upload.single("avatar"), createUser);
+router.post("/activation", activateUser);
 
 export default router;
