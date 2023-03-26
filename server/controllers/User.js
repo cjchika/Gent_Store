8f82f3d2358aa91f727706649e9edd0c6f1eee9a
Chijioke Chika <8pluscreative@gmail.com>
2023-03-26 16:26:17 +0100

    User

--- server/controllers/User.js
+++ server/controllers/User.js
@@ -2,6 +2,21 @@ import path from "path";
 import User from "../models/User.js";
 import Product from "../models/Product.js";
 import ErrorHandler from "../handlers/ErrorHandler.js";
+import { asyncErrors } from "../middleware/catchAsyncErrors.js";
+import { upload } from "../multer.js";
+import fs from "fs";
+import jwt from "jsonwebtoken";
+import sendMail from "../handlers/sendMail.js";
+import { userAuth } from "../middleware/Auth.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const createActivationToken = (user) => {
+  return jwt.sign(user, process.env.ACTIVATION_SECRET, {
+    expiresIn: "5m",
+  });
+};
 
 export const createUser = async (req, res, next) => {
   try {
@@ -9,12 +24,19 @@ export const createUser = async (req, res, next) => {
     const userEmail = await User.findOne(email);
 
     if (userEmail) {
+      const fileName = req.file.filename;
+      const filePath = `uploads/${fileName}`;
+      fs.unlink(filePath, (err) => {
+        if (err) {
+          console.log(err);
+          res.status(500).json({ message: "Error deleting file" });
+        }
+      });
       return next(new ErrorHandler("User already exists", 400));
     }
 
     const fileName = req.file.filename;
-    const fileUrl = path.join(fileName);
-    const avatar = fileUrl;
+    const avatar = path.join(fileName);
 
     const user = { name, email, password, avatar };
 
