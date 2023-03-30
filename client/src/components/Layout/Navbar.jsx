9f26780357e394a730656cdcbb310488d79aadff
Chijioke Chika <8pluscreative@gmail.com>
2023-03-30 12:31:07 +0100

    Navbar

---
+++ client/src/components/Layout/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { navItems } from "../../static/data";
+import styles from "../../styles/styles";
+
+const Navbar = ({ active }) => {
+  return (
+    <div className={`block 800px:${styles.normalFlex}`}>
+      {navItems &&
+        navItems.map((nav, index) => (
+          <div className="flex">
+            <Link
+              to={nav.url}
+              className={`${
+                active === index + 1
+                  ? "text-primaryColor"
+                  : "text-black 800px:text-[#fff]"
+              }`}
+            >
+              {nav.title}
+            </Link>
+          </div>
+        ))}
+    </div>
+  );
+};
+
+export default Navbar;
