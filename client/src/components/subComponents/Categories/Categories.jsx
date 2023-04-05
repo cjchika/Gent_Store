a051ac12b5d10122919f5012c8aa589473741b12
Chijioke Chika <8pluscreative@gmail.com>
2023-04-05 08:19:46 +0100

    Categories

--- client/src/components/subComponents/Categories/Categories.jsx
+++ client/src/components/subComponents/Categories/Categories.jsx
@@ -6,24 +6,24 @@ import CategoryItem from "./CategoryItem";
 const Categories = () => {
   return (
     <>
-      <div className="my-10">
-        <h1 className="capitalize px-10 text-xl font-medium">
+      <div className="my-5 mt-10">
+        <h1 className="capitalize px-5 md:px-10 text-xl font-medium">
           Shop our top categories
         </h1>
       </div>
       <div className={`${styles.section} hidden sm:block`}>
         <div
-          className={`my-12 flex justify-between w-full shadow-sm bg-white p-5 rounded-md `}
+          className={`my-8 flex justify-between w-full shadow-sm bg-white p-3 lg:p-4 rounded-md `}
         >
           {brandingData &&
             brandingData.map((item, index) => (
-              <div className="flex items-center gap-3" key={index}>
+              <div className="flex items-center lg:gap-3" key={index}>
                 <item.icon className="text-4xl text-secColor" />
                 <div className="px-3">
-                  <h3 className="font-bold text-priColor text-sm md:text-lg">
+                  <h3 className="font-bold text-priColor text-xs xl:text-lg">
                     {item.title}
                   </h3>
-                  <p className="text-xs md:text-sm text-secColor font-medium">
+                  <p className="text-xs xl:text-sm text-secColor xl:font-medium">
                     {item.Description}
                   </p>
                 </div>
--- client/src/components/subComponents/Categories/CategoryItem.jsx
+++ client/src/components/subComponents/Categories/CategoryItem.jsx
@@ -13,65 +13,63 @@ import "swiper/swiper-bundle.css";
 SwiperCore.use([Pagination, Controller, Thumbs, Scrollbar, Autoplay]);
 
 const CategoryItem = () => {
+  const handleSelect = (item) => {
+    navigate(`/products?category=${item}`);
+  };
+
   const navigate = useNavigate();
+
   return (
-    <div className={` bg-white p-6 px-8 rounded-lg mb-5 flex`} id="categories">
-      {categories &&
-        categories.map((item, index) => {
-          const handleSubmit = (i) => {
-            navigate(`/products?category=${item.title}`);
-          };
-          return (
-            <Swiper
-              modules={Autoplay}
-              grabCursor={true}
-              id="sec"
-              loop={true}
-              autoplay={{
-                delay: 4000,
-                disableOnInteraction: false,
-              }}
-              // breakpoints={{
-              //   0: {
-              //     slidesPerView: 2,
-              //     spaceBetween: 5,
-              //     cssMode: true,
-              //   },
-              //   768: {
-              //     slidesPerView: 3,
-              //     spaceBetween: 10,
-              //     cssMode: true,
-              //   },
-              //   900: {
-              //     slidesPerView: 4,
-              //     spaceBetween: 20,
-              //   },
-              //   1200: {
-              //     slidesPerView: 5,
-              //     spaceBetween: 25,
-              //   },
-              //   1700: {
-              //     slidesPerView: 6,
-              //     spaceBetween: 30,
-              //   },
-              // }}
-            >
-              <SwiperSlide
-                key={index}
-                onClick={handleSubmit}
-                className="cursor-pointer"
-              >
-                <div className="bg-priColor bg-opacity-20 rounded-lg w-[100px] h-[100px]">
-                  <img
-                    src={item.img}
-                    className="w-full h-full object-contain mx-auto"
-                  />
-                </div>
-                <h1 className="text-center">{item.title}</h1>
-              </SwiperSlide>
-            </Swiper>
-          );
-        })}
+    <div className={` bg-white p-5 rounded-lg mb-5 `} id="categories">
+      <Swiper
+        grabCursor={true}
+        id="main"
+        loop={true}
+        autoplay={{
+          delay: 4000,
+          disableOnInteraction: false,
+        }}
+        breakpoints={{
+          0: {
+            slidesPerView: 3,
+            spaceBetween: 5,
+          },
+          768: {
+            slidesPerView: 6,
+            spaceBetween: 5,
+          },
+          900: {
+            slidesPerView: 7,
+            spaceBetween: 5,
+          },
+          1200: {
+            slidesPerView: 9,
+            spaceBetween: 5,
+          },
+          1700: {
+            slidesPerView: 9,
+            spaceBetween: 5,
+          },
+        }}
+      >
+        {categories.map((item, index) => (
+          <SwiperSlide
+            key={index}
+            onClick={() => handleSelect(item.title)}
+            className="cursor-pointer "
+          >
+            <div className="mx-auto bg-priColor bg-opacity-10 hover:bg-opacity-20 rounded-lg w-[100px] h-[100px]">
+              <img
+                src={item.img}
+                className="w-[75%] hover:w-[90%] h-full object-contain mx-auto"
+              />
+            </div>
+            <h1 className="text-center text-secColor mt-2 lg:font-semibold">
+              {item.title}
+            </h1>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
 };
