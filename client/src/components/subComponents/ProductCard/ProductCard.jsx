a6f7531fac3b72328699ef62ef565f960dbcff26
Chijioke Chika <8pluscreative@gmail.com>
2023-04-06 23:41:55 +0100

    Product Card

--- client/src/components/subComponents/ProductCard/ProductCard.jsx
+++ client/src/components/subComponents/ProductCard/ProductCard.jsx
@@ -11,6 +11,12 @@ import { Link } from "react-router-dom";
 import styles from "../../../styles/styles";
 
 const ProductCard = ({ item }) => {
+  const [click, setClick] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  const p = item.name;
+  const productName = p.replace;
+
   return <div>Product Card</div>;
 };
 
