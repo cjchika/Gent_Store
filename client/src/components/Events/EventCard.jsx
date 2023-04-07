5cc2c44e7c72f819cab78a125227c297ddd5a11a
Chijioke Chika <8pluscreative@gmail.com>
2023-04-07 10:38:46 +0100

    Events

--- client/src/components/Events/EventCard.jsx
+++ client/src/components/Events/EventCard.jsx
@@ -3,33 +3,39 @@ import { baseUrl } from "../../config/api";
 import styles from "../../styles/styles";
 import CountDown from "./CountDown";
 
-const EventCard = ({ active, items }) => {
+const EventCard = ({ active, item }) => {
   return (
     <div
       className={`w-full block bg-white rounded-lg ${
         active ? "unset" : "mb-12"
       } lg:flex p-2`}
     >
-      <div className="w-full lg:-w[50%] m-auto">
-        <img src={`${backend_url}${data.images[0]}`} alt="" />
+      <div className="w-full lg:-w[50%] m-auto mb-7 md:mb-0">
+        <img src={`${item.image_Url[0].url}`} alt="" />
       </div>
-      <div className="w-full lg:[w-50%] flex flex-col justify-center">
-        <h2 className={`${styles.productTitle}`}>{data.name}</h2>
-        <p>{data.description}</p>
+      <div className="w-full lg:[w-50%] flex flex-col justify-center lg:pr-3">
+        <h2
+          className={`${styles.productTitle} capitalize mb-3 text-deepSecColor`}
+        >
+          {item.name}
+        </h2>
+        <p className="text-justify mb-5 text-secColor text-opacity-80">
+          {item.description}
+        </p>
         <div className="flex py-2 justify-between">
           <div className="flex">
             <h5 className="font-[500] text-[18px] text-[#d55b45] pr-3 line-through">
-              {data.originalPrice}$
+              ${item.originalPrice}
             </h5>
-            <h5 className="font-bold text-[20px] text-[#333] font-Roboto">
-              {data.discountPrice}$
+            <h5 className="font-bold text-[20px] text-priColor text-2xl font-Roboto">
+              ${item.discountPrice}
             </h5>
           </div>
-          <span className="pr-3 font-[400] text-[17px] text-[#44a55e]">
+          <span className="pr-3 font-[400] text-[17px] text-priColor">
             120 sold
           </span>
         </div>
-        <CountDown data={data} />
+        <CountDown item={item} />
         <br />
       </div>
     </div>
--- client/src/components/Events/Events.jsx
+++ client/src/components/Events/Events.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import styles from "../../styles/styles";
+import EventCard from "./EventCard";
+import { productData } from "../../static/data";
 
 const Events = () => {
-  return <div>Events</div>;
+  const [allEvents, setAllEvents] = useState(productData[0]);
+  // const { allEvents } = useSelector((state) => state.events);
+  const [isLoading, setIsLoading] = useState(false);
+
+  return (
+    <div>
+      {!isLoading && (
+        <div className={`${styles.section}`}>
+          <div className={`${styles.heading}`}>
+            <h1 className="text-secColor">Featured Product</h1>
+          </div>
+
+          <div className="w-full grid">
+            <EventCard item={allEvents} />
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Events;
