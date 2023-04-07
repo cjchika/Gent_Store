90397d93d7f51c1f56792f9e6afc4571e935e5e5
Chijioke Chika <8pluscreative@gmail.com>
2023-04-07 10:43:34 +0100

    Countdown

--- client/src/components/Events/CountDown.jsx
+++ client/src/components/Events/CountDown.jsx
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const CountDown = () => {
-  return <div>CountDown</div>;
+const CountDown = ({ item }) => {
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+    return () => clearTimeout(timer);
+  });
+
+  function calculateTimeLeft() {
+    const difference = +new Date(item.Finish_Date) - +new Date();
+    let timeLeft = {};
+
+    if (difference > 0) {
+      timeLeft = {
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60),
+      };
+    }
+
+    return timeLeft;
+  }
+
+  const timerComponents = Object.keys(timeLeft).map((interval) => {
+    if (!timeLeft[interval]) {
+      return null;
+    }
+
+    return (
+      <span className="text-[25px] text-secColor">
+        {timeLeft[interval]} {interval}{" "}
+      </span>
+    );
+  });
+
+  return (
+    <div>
+      {timerComponents.length ? (
+        timerComponents
+      ) : (
+        <span className="text-[red] text-base">Promo Closed</span>
+      )}
+    </div>
+  );
 };
 
 export default CountDown;
