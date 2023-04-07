a01a6301af0e3242e32fd75804871c44204f45a2
Chijioke Chika <8pluscreative@gmail.com>
2023-04-07 07:33:55 +0100

    Product card

--- client/src/components/subComponents/ProductCard/ProductCard.jsx
+++ client/src/components/subComponents/ProductCard/ProductCard.jsx
@@ -19,69 +19,76 @@ const ProductCard = ({ item }) => {
 
   return (
     <>
-      <div className="w-full h-[350px] bg-white rounded-lg shadow-sm p-3 relative cursor-pointer">
-        <div className="flex justify-end"></div>
-        <Link to={`/product/${productName}`}>
-          <img
-            className="w-full h-[170px] object-contain"
-            src={item.image_Url[0].url}
-            alt="product image"
-          />
-        </Link>
-        <Link to="/">
-          <h5 className={`${styles.shop_name} text-secColor`}>
-            {item.shop.name}
-          </h5>
-        </Link>
-        <Link to="/">
-          <h4>
-            {item.name.length > 40 ? item.name.slice(0, 40) + "..." : item.name}
-          </h4>
+      <div className="w-full h-auto bg-white border border-opacity-10 hover:border-opacity-40 hover:border-priColor border-secColor rounded-lg shadow-sm  relative cursor-pointer">
+        <div className="p-3">
+          <Link to={`/product/${productName}`}>
+            <img
+              className="w-full h-[170px] object-contain"
+              src={item.image_Url[0].url}
+              alt="product image"
+            />
+          </Link>
+        </div>
+        <div className="bg-priColor bg-opacity-10 rounded-b-lg  px-3">
+          <Link to="/">
+            <h5
+              className={`${styles.shop_name} text-secColor font-semibold text-xs`}
+            >
+              {item.shop.name}
+            </h5>
+          </Link>
+          <Link to="/">
+            <h4 className="text-sm text-deepSecColor">
+              {item.name.length > 50
+                ? item.name.slice(0, 50) + "..."
+                : item.name}
+            </h4>
 
-          <div className="flex">
-            <AiFillStar
-              className="mr-2 cursor-pointer"
-              size={15}
-              color="#F6BA00"
-            />
-            <AiFillStar
-              className="mr-2 cursor-pointer"
-              size={15}
-              color="#F6BA00"
-            />
-            <AiFillStar
-              className="mr-2 cursor-pointer"
-              size={15}
-              color="#F6BA00"
-            />
-            <AiFillStar
-              className="mr-2 cursor-pointer"
-              color="#F6BA00"
-              size={15}
-            />
-            <AiOutlineStar
-              size={15}
-              className="mr-2 cursor-pointer"
-              color="#F6BA00"
-            />
-          </div>
-          <div className="py-2 flex items-center justify-between">
-            <div className="flex">
-              <h5 className={`${styles.productDiscountPrice}`}>
-                {item.originalPrice === 0
-                  ? item.originalPrice
-                  : item.discountPrice}
-                $
-              </h5>
-              <h4 className={`${styles.price}`}>
-                {item.originalPrice ? item.originalPrice + " $" : null}
-              </h4>
+            <div className="flex mb-2 mt-3">
+              <AiFillStar
+                className="mr-2 cursor-pointer"
+                size={16}
+                color="#F6BA00"
+              />
+              <AiFillStar
+                className="mr-2 cursor-pointer"
+                size={16}
+                color="#F6BA00"
+              />
+              <AiFillStar
+                className="mr-2 cursor-pointer"
+                size={16}
+                color="#F6BA00"
+              />
+              <AiFillStar
+                className="mr-2 cursor-pointer"
+                color="#F6BA00"
+                size={16}
+              />
+              <AiOutlineStar
+                size={16}
+                className="mr-2 cursor-pointer"
+                color="#F6BA00"
+              />
             </div>
-            <span className="font-[400] text-[17px] text-[#68d284]">
-              50 sold
-            </span>
-          </div>
-        </Link>
+            <div className="py-2 flex items-center justify-between">
+              <div className="flex">
+                <h5
+                  className={`${styles.productDiscountPrice} text-deepSecColor text-base`}
+                >
+                  $
+                  {item.originalPrice === 0
+                    ? item.originalPrice
+                    : item.discountPrice}
+                </h5>
+                <h4 className={`${styles.price} text-xs`}>
+                  {item.originalPrice ? "$" + item.originalPrice : null}
+                </h4>
+              </div>
+              <span className="font-[400] text-sm text-priColor">50 sold</span>
+            </div>
+          </Link>
+        </div>
 
         {/* side options */}
         <div>
