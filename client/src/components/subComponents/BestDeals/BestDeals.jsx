3a2b8822f37373a5723836ed136d41e389a65593
Chijioke Chika <8pluscreative@gmail.com>
2023-04-07 06:52:21 +0100

    Product card

--- client/src/components/subComponents/BestDeals/BestDeals.jsx
+++ client/src/components/subComponents/BestDeals/BestDeals.jsx
@@ -9,7 +9,7 @@ const BestDeals = () => {
   useEffect(() => {
     const products =
       productData && productData.sort((a, b) => b.total_sell - a.total_sell);
-    setData(products.slice(0, 10));
+    setData(products.slice(0, 5));
   }, []);
 
   return (
@@ -18,7 +18,7 @@ const BestDeals = () => {
         <div className={`${styles.heading}`}>
           <h1 className="text-secColor">Best Deals</h1>
         </div>
-        <div className="flex flex-wrap gap-3">
+        <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-4 lg:gap-[25px] xl:grid-cols-5 xl:gap-[30px] mb-12 border-0">
           {data &&
             data.map((item, index) => <ProductCard item={item} key={index} />)}
         </div>
--- client/src/components/subComponents/ProductCard/ProductCard.jsx
+++ client/src/components/subComponents/ProductCard/ProductCard.jsx
@@ -19,7 +19,7 @@ const ProductCard = ({ item }) => {
 
   return (
     <>
-      <div className="w-full h-[370px] bg-white rounded-lg shadow-sm p-3 relative cursor-pointer">
+      <div className="w-full h-[350px] bg-white rounded-lg shadow-sm p-3 relative cursor-pointer">
         <div className="flex justify-end"></div>
         <Link to={`/product/${productName}`}>
           <img
@@ -28,6 +28,100 @@ const ProductCard = ({ item }) => {
             alt="product image"
           />
         </Link>
+        <Link to="/">
+          <h5 className={`${styles.shop_name} text-secColor`}>
+            {item.shop.name}
+          </h5>
+        </Link>
+        <Link to="/">
+          <h4>
+            {item.name.length > 40 ? item.name.slice(0, 40) + "..." : item.name}
+          </h4>
+
+          <div className="flex">
+            <AiFillStar
+              className="mr-2 cursor-pointer"
+              size={15}
+              color="#F6BA00"
+            />
+            <AiFillStar
+              className="mr-2 cursor-pointer"
+              size={15}
+              color="#F6BA00"
+            />
+            <AiFillStar
+              className="mr-2 cursor-pointer"
+              size={15}
+              color="#F6BA00"
+            />
+            <AiFillStar
+              className="mr-2 cursor-pointer"
+              color="#F6BA00"
+              size={15}
+            />
+            <AiOutlineStar
+              size={15}
+              className="mr-2 cursor-pointer"
+              color="#F6BA00"
+            />
+          </div>
+          <div className="py-2 flex items-center justify-between">
+            <div className="flex">
+              <h5 className={`${styles.productDiscountPrice}`}>
+                {item.originalPrice === 0
+                  ? item.originalPrice
+                  : item.discountPrice}
+                $
+              </h5>
+              <h4 className={`${styles.price}`}>
+                {item.originalPrice ? item.originalPrice + " $" : null}
+              </h4>
+            </div>
+            <span className="font-[400] text-[17px] text-[#68d284]">
+              50 sold
+            </span>
+          </div>
+        </Link>
+
+        {/* side options */}
+        <div>
+          {click ? (
+            <AiFillHeart
+              size={20}
+              className="cursor-pointer absolute right-2 top-5"
+              onClick={() => setClick(!click)}
+              color={click ? "red" : "#0b2a3f"}
+              title="Remove from wishlist"
+            />
+          ) : (
+            <AiOutlineHeart
+              size={20}
+              className="cursor-pointer absolute right-2 top-5"
+              onClick={() => setClick(!click)}
+              color={click ? "red" : "#0b2a3f"}
+              title="Add to wishlist"
+            />
+          )}
+          <AiOutlineEye
+            size={20}
+            className="cursor-pointer absolute right-2 top-14"
+            onClick={() => setOpen(!open)}
+            color="#0b2a3f"
+            title="Quick view"
+          />
+          <AiOutlineShoppingCart
+            size={20}
+            className="cursor-pointer absolute right-2 top-24"
+            onClick={() => setOpen(!open)}
+            color="#0b2a3f"
+            title="Add to cart"
+          />
+          {/* {
+                open ? (
+                    <ProductDetailsCard setOpen={setOpen} data={data} />
+                ) : null
+               } */}
+        </div>
       </div>
     </>
   );
