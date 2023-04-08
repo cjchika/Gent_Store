4a9c5bf3874e085635791af8db37284aab550f8f
Chijioke Chika <8pluscreative@gmail.com>
2023-04-08 21:32:36 +0100

    Profile Page

--- client/src/components/Profile/ProfileContent.jsx
+++ client/src/components/Profile/ProfileContent.jsx
@@ -37,7 +37,90 @@ const ProfileContent = ({ active }) => {
                 alt="user"
                 className="w-[100px] h-[100px] rounded-full object-cover border-2 border-priColor"
               />
+              <div className="text-secColor w-[30px] h-[30px] bg-[#E3E9EE] rounded-full flex items-center justify-center cursor-pointer absolute bottom-[5px] right-[5px]">
+                <AiOutlineCamera />
+              </div>
             </div>
+          </div>
+          <br />
+          <br />
+          <div className="w-full px-5">
+            <form onSubmit={handleSubmit} aria-required={true}>
+              <div className="w-full 800px:flex block pb-3">
+                <div className="w-full 800px:w-[50%]">
+                  <label className="block pb-2">Full Name</label>
+                  <input
+                    type="text"
+                    className={`${styles.input} !w-[95%] py-3 border-secColor mb-4 800px:mb-0`}
+                    required
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                </div>
+                <div className=" w-[100%] 800px:w-[50%]">
+                  <label className="block pb-2">Email Address</label>
+                  <input
+                    type="text"
+                    className={`${styles.input} !w-[95%] py-3 border-secColor mb-1 800px:mb-0`}
+                    required
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
+              </div>
+
+              <div className="w-full 800px:flex block pb-3">
+                <div className=" w-[100%] 800px:w-[50%]">
+                  <label className="block pb-2">Phone Number</label>
+                  <input
+                    type="number"
+                    className={`${styles.input} !w-[95%] py-3 border-secColor mb-4 800px:mb-0`}
+                    required
+                    value={phoneNumber}
+                    onChange={(e) => setPhoneNumber(e.target.value)}
+                  />
+                </div>
+                <div className=" w-[100%] 800px:w-[50%]">
+                  <label className="block pb-2">Zip Code</label>
+                  <input
+                    type="number"
+                    className={`${styles.input} !w-[95%] py-3 border-secColor mb-4 800px:mb-0`}
+                    required
+                    value={zipCode}
+                    onChange={(e) => setZipCode(e.target.value)}
+                  />
+                </div>
+              </div>
+
+              <div className="w-full 800px:flex block pb-3">
+                <div className=" w-[100%] 800px:w-[50%]">
+                  <label className="block pb-2">Address 1</label>
+                  <input
+                    type="address"
+                    className={`${styles.input} !w-[95%] py-3 border-secColor`}
+                    required
+                    value={address1}
+                    onChange={(e) => setAddress1(e.target.value)}
+                  />
+                </div>
+                <div className=" w-[100%] 800px:w-[50%]">
+                  <label className="block pb-2">Address 2</label>
+                  <input
+                    type="address"
+                    className={`${styles.input} !w-[95%] py-3 border-secColor`}
+                    required
+                    value={address2}
+                    onChange={(e) => setAddress2(e.target.value)}
+                  />
+                </div>
+              </div>
+              <button
+                type="submit"
+                className={`p-3 px-20 w-[95%] 800px:w-auto border border-secColor text-center text-secColor hover:bg-secColor hover:bg-opacity-5 rounded-lg mt-8`}
+              >
+                Update
+              </button>
+            </form>
           </div>
         </>
       )}
