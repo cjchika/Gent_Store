58a3c77cab3179d3b2d8390ead8b4c584e8af088
Chijioke Chika <8pluscreative@gmail.com>
2023-04-08 13:15:06 +0100

    Payment Page/Refactor

--- client/src/components/Checkout/CheckoutSteps.jsx
+++ client/src/components/Checkout/CheckoutSteps.jsx
@@ -6,14 +6,14 @@ const CheckoutSteps = ({ active }) => {
     <div className="w-full flex justify-center mx-auto">
       <div className="w-[90%] 800px:w-[80%] flex justify-center items-center flex-wrap">
         <div className={`${styles.normalFlex}`}>
-          <div className={`${styles.cart_button}`}>
+          <div className={`${styles.cart_button} !bg-priColor`}>
             <span className={`${styles.cart_button_text}`}>1. Shipping</span>
           </div>
           <div
             className={`${
               active > 1
-                ? "w-[30px] 800px:w-[70px] h-[4px] !bg-[#f63b60]"
-                : "w-[30px] 800px:w-[70px] h-[4px] !bg-[#FDE1E6]"
+                ? "w-[30px] 800px:w-[70px] h-[4px] !bg-priColor"
+                : "w-[30px] 800px:w-[70px] h-[4px] !bg-[#05af6e] !bg-opacity-10"
             }`}
           />
         </div>
@@ -23,15 +23,15 @@ const CheckoutSteps = ({ active }) => {
           <div
             className={`${
               active > 2
-                ? `${styles.cart_button}`
-                : `${styles.cart_button} !bg-[#FDE1E6]`
+                ? `${styles.cart_button} !bg-priColor`
+                : `${styles.cart_button} !bg-[#05af6e] !bg-opacity-10`
             }`}
           >
             <span
               className={`${
                 active > 2
                   ? `${styles.cart_button_text}`
-                  : `${styles.cart_button_text} !text-[#f63b60]`
+                  : `${styles.cart_button_text} !text-priColor`
               }`}
             >
               2. Payment
@@ -43,22 +43,22 @@ const CheckoutSteps = ({ active }) => {
           <div
             className={`${
               active > 3
-                ? "w-[30px] 800px:w-[70px] h-[4px] !bg-[#f63b60]"
-                : "w-[30px] 800px:w-[70px] h-[4px] !bg-[#FDE1E6]"
+                ? "w-[30px] 800px:w-[70px] h-[4px] !bg-priColor"
+                : "w-[30px] 800px:w-[70px] h-[4px] !bg-[#05af6e] !bg-opacity-10"
             }`}
           />
           <div
             className={`${
               active > 3
-                ? `${styles.cart_button}`
-                : `${styles.cart_button} !bg-[#FDE1E6]`
+                ? `${styles.cart_button} !bg-priColor`
+                : `${styles.cart_button} !bg-[#05af6e] !bg-opacity-10`
             }`}
           >
             <span
               className={`${
                 active > 3
-                  ? `${styles.cart_button_text}`
-                  : `${styles.cart_button_text} !text-[#f63b60]`
+                  ? `${styles.cart_button_text} `
+                  : `${styles.cart_button_text} !text-priColor`
               }`}
             >
               3. Success
