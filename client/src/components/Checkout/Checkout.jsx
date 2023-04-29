46efeea3f329c1892e58fba7507ff2f2f5eb9157
Chijioke Chika <8pluscreative@gmail.com>
2023-04-29 09:31:39 +0100

    User

--- client/src/components/Checkout/Checkout.jsx
+++ client/src/components/Checkout/Checkout.jsx
@@ -19,6 +19,88 @@ const Checkout = () => {
   const [discountPrice, setDiscountPrice] = useState(null);
   const navigate = useNavigate();
 
+  const paymentSubmit = () => {
+    if (
+      address1 === "" ||
+      address2 === "" ||
+      zipCode === null ||
+      country === "" ||
+      city === ""
+    ) {
+      toast.error("Please choose your delivery address!");
+    } else {
+      const shippingAddress = {
+        address1,
+        address2,
+        zipCode,
+        country,
+        city,
+      };
+
+      const orderData = {
+        cart,
+        totalPrice,
+        subTotalPrice,
+        shipping,
+        discountPrice,
+        shippingAddress,
+        user,
+      };
+
+      // update local storage with the updated orders array
+      localStorage.setItem("latestOrder", JSON.stringify(orderData));
+      navigate("/payment");
+    }
+  };
+
+  const subTotalPrice = cart.reduce(
+    (acc, item) => acc + item.qty * item.discountPrice,
+    0
+  );
+
+  // SHIPPING COST
+  const shipping = subTotalPrice * 0.1;
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const name = couponCode;
+
+    await axios.get(`${server}/coupon/get-coupon-value/${name}`).then((res) => {
+      const shopId = res.data.couponCode?.shopId;
+      const couponCodeValue = res.data.couponCode?.value;
+      if (res.data.couponCode !== null) {
+        const isCouponValid =
+          cart && cart.filter((item) => item.shopId === shopId);
+
+        if (isCouponValid.length === 0) {
+          toast.error("Coupon code is not valid for this shop");
+          setCouponCode("");
+        } else {
+          const eligiblePrice = isCouponValid.reduce(
+            (acc, item) => acc + item.qty * item.discountPrice,
+            0
+          );
+          const discountPrice = (eligiblePrice * couponCodeValue) / 100;
+          setDiscountPrice(discountPrice);
+          setCouponCodeData(res.data.couponCode);
+          setCouponCode("");
+        }
+      }
+      if (res.data.couponCode === null) {
+        toast.error("Coupon code doesn't exists!");
+        setCouponCode("");
+      }
+    });
+  };
+
+  const discountPercentage = couponCodeData ? discountPrice : "";
+
+  const totalPrice = couponCodeData
+    ? (subTotalPrice + shipping - discountPercentage).toFixed(2)
+    : (subTotalPrice + shipping).toFixed(2);
+
+  console.log(discountPercentage);
+
   return (
     <div className="w-fll flex flex-col items-center py-8">
       <div className="w-[90%] 1000px:w-[70%] block 800px:flex">
@@ -40,7 +122,15 @@ const Checkout = () => {
           />
         </div>
         <div className="w-full 800px:w-[35%] 800px:mt-0 mt-8">
-          <CartContent />
+          <CartContent
+            handleSubmit={handleSubmit}
+            totalPrice={totalPrice}
+            shipping={shipping}
+            subTotalPrice={subTotalPrice}
+            couponCode={couponCode}
+            setCouponCode={setCouponCode}
+            discountPercentage={discountPercentage}
+          />
         </div>
       </div>
       <button
