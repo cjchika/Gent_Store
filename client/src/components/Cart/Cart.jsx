90fdd6e5ee8aa1444a130582bcc373b8a3154017
Chijioke Chika <8pluscreative@gmail.com>
2023-05-01 06:52:55 +0100

    Fixes

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -28,7 +28,7 @@ const Cart = ({ setOpenCart }) => {
   return (
     <div className="fixed top-0 left-0 w-full bg-[#0000004b] h-screen z-10">
       <div className="fixed top-0 right-0 min-h-full w-[60%] md:w-[40%] lg:w-[25%] bg-white flex flex-col justify-between shadow-sm">
-        <div className="overflow-scroll">
+        <div>
           <div className="flex w-full justify-end pt-5 pr-5">
             <RxCross1
               size={16}
